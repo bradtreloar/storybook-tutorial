25ca52c06e39519fd17ee1efc2b4d0491b4e6e7d
Brad Treloar <brad@treloardigital.com.au>
2021-03-13 10:06:26 +1030

    add overflow ellipsis to task title

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -38,3 +38,13 @@ Archived.args = Object.assign({}, Default.args, {
     state: 'TASK_ARCHIVED',
   },
 });
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = Template.bind({});
+LongTitle.args = Object.assign({}, Default.args, {
+  task: {
+    ...Default.args.task,
+    title: longTitleString,
+  },
+});
